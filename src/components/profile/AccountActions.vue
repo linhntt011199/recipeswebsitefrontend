cb435a7851a0f179ce08028b8f37d7fcd0271587
Thanhha0426 <nguyenthanhha041999@gmail.com>
2021-01-19 00:03:26 +0700

    add profile page

---
+++ src/components/profile/AccountActions.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="mx-auto account-actions">
+    <h4 class="heading">Account management</h4>
+    <div class="btns">
+      <update-password-form />
+      <delete-account />
+    </div>
+  </v-card>
+</template>
+
+<script>
+import UpdatePasswordForm from "@/components/forms/profile/UpdatePassword";
+import DeleteAccount from "./DeleteAccount";
+export default {
+  name: "account-actions",
+  components: {
+    UpdatePasswordForm,
+    DeleteAccount
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../scss/style";
+.account-actions {
+  padding: 2rem;
+  margin-bottom: 2rem;
+}
+.heading {
+  color: $app-secondary-color;
+  text-transform: uppercase;
+  margin-bottom: 0.5rem;
+}
+.btns {
+  display: flex;
+}
+.btn {
+  &--delete {
+    margin-left: 1rem;
+  }
+}
+</style>
